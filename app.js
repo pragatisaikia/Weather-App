d75a284eb80adfd769a508a901dc381d65f8d83c
pragatisaikia <pragatisaikia2003@gmail.com>
2023-02-11 00:35:25 +0530

    temperature-conversion

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+const notifyElement = document.querySelector(".notify");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature p");
+const descElement = document.querySelector(".temp-desc p");
+const locationElement = document.querySelector(".location p");
 
+const weather = {};
+
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`
+    tempElement.innerHTML = `${weather.temperature.value} degree<span> C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city},${weather.country}`;
+}
+
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+tempElement.addEventListener("click",function(){
+    if(weather.temperature.value === undefined) return;
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        tempElement.innerHTML = `${fahrenheit} degree <span> F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value} degree <span> C</span>`;
+        weather.temperature.value = "celsius";
+    }
+})
