c5ff43c6efbf8380e24bf15036928497b46be299
pragatisaikia <pragatisaikia2003@gmail.com>
2023-02-11 01:36:02 +0530

    fetched data

--- app.js
+++ app.js
@@ -5,6 +5,52 @@ const descElement = document.querySelector(".temp-desc p");
 const locationElement = document.querySelector(".location p");
 
 const weather = {};
+const key = "4943d1a31d77050417bfb053cc0176fa";
+
+weather.temperature = {
+    unit : "celsius"
+}
+
+const KELVIN = 273;
+
+
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notifyElement.style.display = "block";
+    notifyElement.innerHTML = "<p>Browser doesn't support Geolocation</p>"
+}
+
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    getWeather(latitude,longitude);
+}
+
+function showError(error){
+    notifyElement.style.display = "block";
+    notifyElement.innerHTML = `<p>${error.message}</p>`
+}
+
+function getWeather(latitude,longitude) {
+    let api = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        })
+}
 
 function displayWeather(){
     iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`
